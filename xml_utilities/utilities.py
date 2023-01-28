a7e039acaf2faf5b4448eb5236be1d59dfa8bfa6
Tom Vazquez <elevatorstest@gmail.com>
2023-01-28 14:58:21 -0700

    Simple XML parser and validator for PIES & ACES

---
+++ xml_utilities/utilities.py
@@ -0,0 +1,173 @@
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element
+from pyodbc import Cursor
+import pyodbc
+import pandas as pd
+from pandas import DataFrame
+import os
+import json
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
+
+# Set Up ENV Variables
+db = os.environ["DB"]
+server = os.environ["SERVER"]
+tmp = open(os.environ["TABLES"])
+imprt = open(os.environ["IMPORT"])
+
+tmp_table = json.loads(tmp.read())
+imp_table = json.loads(imprt.read())
+
+conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                      f"Server={server};"
+                      f"Database={db};"
+                      "Trusted_Connection=yes;")
+
+def create_insert_query(table: dict) -> str:
+    cols = list(table.values())[0]
+    table_name = list(table.keys())[0]
+    cols_num = len(cols)
+    values = ['?']*cols_num
+    insert_cols = ''.join(str(val) + ', ' if idx < len(cols) -
+                          1 else str(val) for idx, val in enumerate(cols))
+    insert_values = ''.join(str(val) + ', ' if idx < len(values) -
+                            1 else str(val) for idx, val in enumerate(values))
+    query = f"INSERT INTO {table_name} ({insert_cols}) VALUES ({insert_values})"
+
+    return query
+
+def import_to_database(cursor: Cursor, import_rows: list, query: str) -> str:
+    num_rows = len(import_rows)
+    if num_rows == 1:
+        try:
+            cursor.execute(query, import_rows)
+            return "OK"
+        except Exception as err:
+            return err
+    else:
+        try:
+            cursor.executemany(query, import_rows)
+            return "OK"
+        except Exception as err:
+            return err
+
+def start_import(cursor: Cursor, tables: dict) -> None:
+
+    for key, value in tables.items():
+        table = {key: value}
+        query = create_insert_query(table=table)
+
+        response = import_to_database(
+            cursor=cursor, import_rows=[], query=query)
+
+        if response == "OK":
+            message = f"Import for {key} completed succesfully!"
+            print(message)
+        else:
+            message = f"An error has occurred while importing in to {key}. Error: {response}"
+            print(message)
+
+def get_headers_info(element: Element, namespace: str) -> list:
+    elements = []
+    for element in element:
+        key = element.tag
+        key = key.replace(namespace, '')
+        elements.append({key: element.text})
+    print(elements)
+
+def has_sub(element:Element)->bool:
+    sub_elements = [sub for sub in element]
+    if len(sub_elements) > 0:
+        return True
+    else:
+        return False
+
+def get_sub_elements(flag: bool, element: Element, details: dict, namespace: str) -> None:
+    if flag == True:
+        for sub in element:
+            primary_tag = element.tag
+            primary_tag = primary_tag.replace(namespace, '')
+            if primary_tag == 'DigitalFileInformation':
+
+                sub_digital = {s.tag.replace(namespace, ''): s.text.replace(
+                    namespace, '') for s in element}
+
+                record = {
+                    "PartNumber": details['PartNumber'],
+                    "BrandAAIAID": details['BrandAAIAID'],
+                    "PartTerminologyID": details['PartTerminologyID'],
+                    "Segment": primary_tag
+                }
+
+                record.update(sub_digital)
+                details['records'].append(record)
+            else:
+                get_sub_elements(has_sub(sub), sub, details, namespace)
+    else:
+        key = element.tag
+        key = key.replace(namespace, '')
+
+        if key in details:
+            if len(element.attrib) > 0:
+                text = element.text
+                record = {
+                    "PartNumber": details['PartNumber'],
+                    "BrandAAIAID": details['BrandAAIAID'],
+                    "PartTerminologyID": details['PartTerminologyID'],
+                    "Segment": key,
+                    "Value": text if text != '' else None
+                }
+                record.update(element.attrib)
+                details['records'].append(record)
+
+                return
+            else:
+                details[key] = element.text
+                return
+
+def create_product_list(root: Element, namespace: str) -> list:
+    item_elements = root.findall(f"./{namespace}Items/{namespace}Item")
+    product_list = []
+    for element in item_elements:
+        details = {
+            "PartNumber": "",
+            "BrandAAIAID": "",
+            "PartTerminologyID": "",
+            "Description": "",
+            "ExtendedProductInformation": "",
+            "ProductAttribute": "",
+            "DigitalFileInformation": "",
+            "records": []
+        }
+
+        get_sub_elements(has_sub(element=element), element,
+                         details, namespace=namespace)
+
+        product_list.extend(details['records'])
+
+    return product_list
+
+def prepare_insert(dataFrame: DataFrame, segment: str) -> DataFrame:
+    results = dataFrame.query('Segment == "%s"' % segment)
+    results = results.dropna(axis=1, how='all')
+    return results
+
+def xml_parse(file_name: str) -> str:
+    namespace = '{http://www.autocare.org}'
+    tree = ET.parse(file_name)
+    root = tree.getroot()
+    data = create_product_list(root=root, namespace=namespace)
+    dataFrame = pd.json_normalize(data)
+
+    for key, val in tmp_table.items():
+        rows = prepare_insert(dataFrame, key)
+        import_row = rows.to_numpy().tolist()
+        query = create_insert_query(val)
+        cursor = conn.cursor()
+        response = import_to_database(
+            cursor=cursor, import_rows=import_row, query=query)
+        print(response)
+
+    return "Import Completed!!"
---
+++ xml_utilities/validator.py
@@ -0,0 +1,135 @@
+from lxml import etree
+import os
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+from dotenv import load_dotenv, find_dotenv
+from utilities import xml_parse
+
+load_dotenv(find_dotenv())
+
+
+# Set up gui theme and size
+    
+root = tk.Tk()
+
+style = ttk.Style()
+
+BG_COLOR = "#299617"
+LBL_COLOR = "#D3D3D3"
+TXT_COLOR = "#DCDCDC"
+FONT = "Helvetica 12 bold"
+TITLE = "XML Validator"
+ICON = os.path.dirname(os.environ["ICON"])
+
+w = 600
+h = 400
+
+root.resizable(False, False)
+
+ws = root.winfo_screenwidth()
+hs = root.winfo_screenheight()
+
+root.iconbitmap(ICON)
+
+x = (ws / 2) - (w / 2)
+y = (hs / 2) - (h / 2)
+
+root.title(TITLE)
+root.geometry("%dx%d+%d+%d" % (w, h, x, y))
+root.config(bg=BG_COLOR)
+
+style.configure("TLabelframe", background=LBL_COLOR)
+style.configure(
+    "TLabelframe.Label", font=FONT, background=LBL_COLOR, color=BG_COLOR
+)
+
+#Functions to handle the selected files.
+def set_schema(obj:dict)->str:
+
+    schema = filedialog.askopenfilename(filetypes=[("Schema Files", "*.xsd")])
+    schema.replace("file:/", "")
+    obj["schema"] = schema
+
+    return f"Selected Schema: {schema}"
+
+def set_file(obj:dict)->str:
+    file = filedialog.askopenfilename(filetypes=[("XML Files", "*.xml")])
+    file.replace("file:/", "")
+    obj["file"] = file
+
+    return f"Selected File: {file}"
+
+def validate_file(obj:dict)->str:
+    file_schema = etree.parse(obj["schema"])
+    schema = etree.XMLSchema(file_schema)
+    parser = etree.XMLParser(schema=schema)
+    try:
+        etree.parse(obj["file"], parser)
+        return "Validation Complete. File has passed validation!"
+    except etree.XMLSyntaxError as e:
+        return e.msg
+
+# Set up gui contents
+def main()->None:
+
+    message_frame = ttk.Labelframe(root, text="Validation Message")
+    message_frame.pack(fill="both", expand=True, side="left")
+    message_frame.propagate(False)
+
+    message = tk.Label(
+        message_frame,
+        text="Make A Selection To Start Validation.",
+        background=LBL_COLOR,
+    )
+    message.pack(fill="both", expand=True)
+    message.propagate(False)
+    message.configure(wraplength=500)
+
+    
+    # Set up holder for files
+    obj = {"file": "", "schema": ""}
+    
+    button_frame = tk.Frame(root, background=LBL_COLOR)
+    button_frame.pack(fill="y", side="right")
+    file_button = tk.Button(
+        button_frame,
+        text="XML File",
+        command=lambda: message.config(text=set_file(obj)),
+        height=3,
+        width=10,
+        background=TXT_COLOR,
+    )
+    file_button.pack(side="top")
+    schema_button = tk.Button(
+        button_frame,
+        text="XSD File",
+        command=lambda: message.config(text=set_schema(obj)),
+        height=3,
+        width=10,
+        background=TXT_COLOR,
+    )
+    schema_button.pack(side="top")
+    validation_button = tk.Button(
+        button_frame,
+        text="Validate",
+        command=lambda: message.config(text=validate_file(obj)),
+        height=3,
+        width=10,
+        background=TXT_COLOR,
+    )
+    validation_button.pack(side="top")
+    parse_button = tk.Button(
+        button_frame,
+        text="Import XML",
+        command=lambda: message.config(text=xml_parse(obj['file'])),
+        height=3,
+        width=10,
+        background=TXT_COLOR,
+    )
+    parse_button.pack(side="bottom")
+    
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
